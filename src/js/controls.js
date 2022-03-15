20575bdd58512f17021f6e1f9bfd0c2dbb73b557
SamehAwad <67038060+SamehAwad@users.noreply.github.com>
2022-03-15 11:55:39 +0200

    Fixing bugs

--- src/js/controls.js
+++ src/js/controls.js
@@ -2,7 +2,14 @@ const container = document.querySelector(".container");
 const boxes = document.querySelectorAll(".tile");
 const display = document.querySelector(".display-player");
 const resetBtn = document.querySelector("#reset");
+const playerXResultElement = document.querySelector(".result-x");
+const playerOResultElement = document.querySelector(".result-o");
+const drawElement = document.querySelector(".result-draw");
+
 let playerTurn = "X";
+let playerXScore = 0;
+let playerOScore = 0;
+let draw = 0;
 let playerXSelectedBoxes = {};
 let playerOSelectedBoxes = {};
 const winningConditions = [
@@ -16,15 +23,27 @@ const winningConditions = [
   [2, 4, 6],
 ];
 
-const checkWinners = (player) => {
+const checkWinners = (player, playerTurn) => {
   for (let i = 0; i < winningConditions.length; i++) {
     let elm = winningConditions[i];
     if (player[elm[0]] === elm[0] && player[elm[1]] === elm[1] && player[elm[2]] === elm[2]) {
       document.getElementById(`${elm[0]}`).style.background = "rgb(20, 77, 24)";
       document.getElementById(`${elm[1]}`).style.background = "rgb(20, 77, 24)";
       document.getElementById(`${elm[2]}`).style.background = "rgb(20, 77, 24)";
+      console.log(playerTurn);
+      if (playerTurn === "X") {
+        playerXScore++;
+        playerXResultElement.innerHTML = playerXScore;
+      } else {
+        playerOScore++;
+        playerOResultElement.innerHTML = playerOScore;
+      }
       container.onclick = null;
     }
+  }
+  if (Object.keys(playerOSelectedBoxes).length + Object.keys(playerXSelectedBoxes).length == 9) {
+    draw++;
+    drawElement.innerHTML = draw;
   }
 };
 
@@ -38,12 +57,12 @@ function actions(e) {
 
     if (playerTurn === "X") {
       playerXSelectedBoxes[id] = parseInt(id);
+      checkWinners(playerXSelectedBoxes, playerTurn);
       playerTurn = "O";
-      checkWinners(playerXSelectedBoxes);
     } else {
       playerOSelectedBoxes[id] = parseInt(id);
+      checkWinners(playerOSelectedBoxes, playerTurn);
       playerTurn = "X";
-      checkWinners(playerOSelectedBoxes);
     }
     // Function Check any winners
     display.innerHTML = playerTurn;
@@ -58,10 +77,15 @@ resetBtn.addEventListener("click", () => {
     elm.innerHTML = "";
     elm.style.background = "#12181b";
   });
+
+  boxes.forEach((elm) => {
+    elm.classList.remove(`player${playerTurn}`);
+    elm.classList.add("playerX");
+  });
   display.classList.remove(`player${playerTurn}`);
+  display.classList.add("playerX");
   playerTurn = "X";
   display.innerHTML = playerTurn;
-  display.classList.add(`playerX`);
   playerXSelectedBoxes = {};
   playerOSelectedBoxes = {};
 });
